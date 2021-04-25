aec386aa3ffbdb28b1a8b1f6ee76c7c3d82db99d
Vincent Z <vinlyzhao@gmail.com>
2021-04-25 09:59:42 +0800

    283 - move zeroes

---
+++ src/main/java/com/leetcode/problems/V283MoveZeroes.java
@@ -0,0 +1,25 @@
+package com.leetcode.problems;
+
+public class V283MoveZeroes {
+    public static void moveZeroes(int[] nums) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12};
+
+        moveZeroes(nums);
+
+        for (int i : nums) {
+            System.out.print(i + " ");
+        }
+    }
+}
